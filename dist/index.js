22bc231e46ea173e830981798475eb8d9de777d3
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-09 10:11:50 +0700

    [ANDY] Fix useSort export (#8)

--- dist/index.js
+++ dist/index.js
@@ -11,4 +11,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./render"), exports);
+__exportStar(require("./useSort"), exports);
 //# sourceMappingURL=index.js.map
